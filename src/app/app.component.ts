3a443caf5e4e04d7fe2e69190b8d5a754e662c81
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2018-02-26 17:47:01 -0500

    Day 3 completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
-
+//@component contains json object 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
+  // styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  //title = 'Angular App';
+  // name:string='Kiran';
+  // city:string='Vizag';
+  msg:string = 'angular app';
+   wishes = function():string{
+     return "good moring folks!";
+  
+   }
 }
+
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
+// })
+// export class AppComponent {
+//   title = 'app';
+// }
