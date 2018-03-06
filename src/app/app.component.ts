89bec3d048ad99898803ad165d71b92f2fadc9ab
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2018-03-06 13:57:25 -0500

    structural directives part 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,20 +5,28 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html'
   // styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
+  e:any={id:1001, ename:"Kiran", job:"Trainer", salary:5500};
+}
+
+
+
+
+// export class AppComponent {
   
-  name:string;
-  gender:string;
-  msg:string;
-  findGender(){
-    if (this.name === undefined || this.name === '')
-      this.msg = "please enter a name";
-    else
-      this.msg = `Hello ${this.gender} ${this.name}`;
-  }
+//   name:string;
+//   gender:string;
+//   msg:string;
+//   findGender(){
+//     if (this.name === undefined || this.name === '')
+//       this.msg = "please enter a name";
+//     else
+//       this.msg = `Hello ${this.gender} ${this.name}`;
+//   }
 
    
-}
+// }
 
 // import { Component } from '@angular/core';
 
