f2c7a905b6f3b9e5bae43f7559586e47c24bf8d7
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2018-03-08 17:49:03 -0500

    Employee CRUD Operations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,19 +8,46 @@ import { Component } from '@angular/core';
 
 
 export class AppComponent{
-  x={};
+  x:any={};
   employees:any[];
+  msg:string='';
   constructor(){
-    this.x = null;
     this.employees = [
     {id:101, ename:'Kiran', job:'Trainer', salary:5500},
     {id:102, ename:'Suhal', job:'Trainer', salary:5300},
     {id:103, ename:'Taran', job:'Programmer', salary:4500},
     {id:104, ename:'Abhole', job:'Developers', salary:3500}
   ];
+  this.msg = 'Total Employee count is ' + this.employees.length; 
   }
   selectRow(i){
-    this.x = this.employees[i];
+//     we are using cloning to get the data as when objects are given values, the memorey address will ng-change
+// and hence the two-way binding won't be possible.
+    this.x = JSON.parse(JSON.stringify(this.employees[i]));
+  }
+
+  addRow(){
+    this.employees.push(this.x);
+    this.msg = 'Total Employee count is ' + this.employees.length;
+    this.x = {};
+    alert("Row added succesfully");
+  }
+  deleteRow(i){
+    
+    if (confirm("Are you sure"))
+      this.employees.splice(i,1);
+    this.msg = 'Total Employee count is ' + this.employees.length; 
+  }
+  updateRow(){
+    var x1:any={};
+    for (var i in this.employees) {
+      if (this.employees[i].id == this.x.id){
+        x1 = this.employees[i];
+        break;
+      }
+    }
+    this.employees[i]=this.x;
+    alert('Row updated successfully');
   }
 }
 
