b72be0d40ced934e67a0ebf108e8d9af2ab6b9b0
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2018-04-04 14:42:54 -0400

    Generate components and prepare to move employees code to Employee component 4 files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { StudentComponent } from './student/student.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeComponent,
+    StudentComponent
   ],
   imports: [
     BrowserModule, FormsModule
