b3aeb93293b13f5f6952059469a2206d836c98a1
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2018-04-04 15:22:48 -0400

    Adding if condition to display Employee  CRUD operaitons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,12 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { EmployeeComponent } from './employee/employee.component';
-import { StudentComponent } from './student/student.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    EmployeeComponent,
-    StudentComponent
+    EmployeeComponent
   ],
   imports: [
     BrowserModule, FormsModule
