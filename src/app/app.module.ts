c2c88e934caae140d54b50337d31538ef8922588
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2018-04-15 17:34:50 -0400

    Parent and Child components were created and App Component html was updated with selectors - app-parent & child-parent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { ParentComponent } from './parent/parent.component';
+import { ChildComponent } from './child/child.component';
 
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ParentComponent,
+    ChildComponent
     
   ],
   imports: [
