cdc8da9ce2e010fdb504ed0f29377f1d8cb40687
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2018-04-15 17:02:09 -0400

    Set up the anglar app for day 6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { EmployeeComponent } from './employee/employee.component';
+
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    EmployeeComponent
+    AppComponent
+    
   ],
   imports: [
     BrowserModule, FormsModule
