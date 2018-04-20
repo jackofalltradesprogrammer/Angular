4fb032430d28273e9e052481107ccb62cc9b9e6a
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2018-04-20 19:35:19 -0400

    Passing data from parent to child - using event emitter

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,12 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-child',
   templateUrl: './child.component.html',
   styleUrls: ['./child.component.css'],
-  inputs:['receivedDataFromParent']
+  inputs:['receivedDataFromParent'],
+  outputs:['childEvent1']
 })
 export class ChildComponent implements OnInit {
+  childEvent1 = new EventEmitter<string>();
+
+  onChange(){
+    this.childEvent1.emit("Hello from Child");
+  }
 
   receivedDataFromParent:string;
   
