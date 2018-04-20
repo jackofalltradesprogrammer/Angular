48b5a99da52ae836e17d87720cfa5cb4173b4e66
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2018-04-20 13:08:37 -0400

    Using inputs as an array to recieve data from parents in teh Child component file

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,14 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import {Input} from '@angular/core';
 
 @Component({
   selector: 'app-child',
   templateUrl: './child.component.html',
-  styleUrls: ['./child.component.css']
+  styleUrls: ['./child.component.css'],
+  inputs:['receivedDataFromParent']
 })
 export class ChildComponent implements OnInit {
 
-  @Input()
   receivedDataFromParent:string;
   
   constructor() { }
