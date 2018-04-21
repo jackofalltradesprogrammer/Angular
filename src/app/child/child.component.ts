f25068258df9ce7ea7328380958c5d9cdc1c9f53
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2018-04-20 20:40:19 -0400

    Passing data from child to parent - part 3 with [ngModel] and using checkbox

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -9,9 +9,15 @@ import { Component, OnInit, EventEmitter } from '@angular/core';
 })
 export class ChildComponent implements OnInit {
   childEvent1 = new EventEmitter<string>();
-  txt1: string;
+  name1: string;
+  terms: boolean;
   onChange(){
-    this.childEvent1.emit(this.txt1);
+    if (this.terms){
+    this.childEvent1.emit('Thank you ' + this.name1 + " for accepting terms"); 
+    }
+    else {
+      this.childEvent1.emit( this.name1 + " read and accept terms"); 
+      }
   }
 
   receivedDataFromParent:string;
